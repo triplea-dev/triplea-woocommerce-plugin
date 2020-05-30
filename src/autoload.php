d762437972d56e84ea97f4b4357af8d893812360
Brian Henry <BrianHenryIE@gmail.com>
2020-05-29 20:18:11 -0700

    Move files to WooCommerce subfolder

--- src/autoload.php
+++ src/autoload.php
@@ -3,7 +3,7 @@
 $class_map = array(
 	'TripleA_Payment_Gateway_For_WooCommerce\WPPB\WPPB_Loader_Interface' => __DIR__ . '/vendor/WPPB/interface-wppb-loader.php',
 	'TripleA_Payment_Gateway_For_WooCommerce\WPPB\WPPB_Loader' => __DIR__ . '/vendor/WPPB/class-wppb-loader.php',
-	'TripleA_Bitcoin_Ecommerce_for_WooCommerce_Payment' => __DIR__ . '/includes/triplea-payment-gateway-main-class.php'
+	'TripleA_Bitcoin_Ecommerce_for_WooCommerce_Payment' => __DIR__ . '/WooCommerce/triplea-payment-gateway-main-class.php'
 );
 
 spl_autoload_register(
