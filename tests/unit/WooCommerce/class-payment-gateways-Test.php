d9d8224ea2e8b7d5641ae4c3180d0882fa593b17
Brian Henry <BrianHenryIE@gmail.com>
2020-06-17 23:23:21 -0700

    Test for registering gateway with WooCommerce

---
+++ tests/unit/WooCommerce/class-payment-gateways-Test.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Tests for I18n. Tests load_plugin_textdomain.
+ *
+ * @package TripleA_Payment_Gateway_For_WooCommerce
+ * @author  TripleA <andy.hoebeke@triple-a.io>
+ */
+
+namespace TripleA_Payment_Gateway_For_WooCommerce\WooCommerce;
+
+/**
+ * Class Payment_Gateways_Test
+ *
+ * @see Payment_Gateways
+ */
+class Payment_Gateways_Test extends \Codeception\Test\Unit {
+
+	/**
+	 * woocommerce_payment_gateways
+	 */
+	public function test_gateway_is_registered_with_woocommerce() {
+
+		$payment_gateways_class = new Payment_Gateways();
+
+		$registered_payment_gateways = $payment_gateways_class->add_triplea_payment_gateway_to_woocommerce( array() );
+
+		$this->assertContains( 'TripleA_Payment_Gateway_For_WooCommerce\WooCommerce\TripleA_Payment_Gateway', $registered_payment_gateways );
+
+	}
+}
