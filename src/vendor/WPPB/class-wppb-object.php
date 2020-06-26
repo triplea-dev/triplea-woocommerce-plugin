29325a1190ef40135ae9b57e5193ac86649ec9f8
Andy Hoebeke <ahoebeke@gmail.com>
2020-06-26 11:41:19 +0800

    Including /src/vendor folder (comment out in gitignore). Needed for sodium compat lib.

---
+++ src/vendor/WPPB/class-wppb-object.php
@@ -0,0 +1,66 @@
+<?php
+
+// Probably consists of public functions that will be hooked as actions or filters
+// and private helper functions.
+
+// api, settings
+namespace TripleA_Payment_Gateway_For_WooCommerce\WPPB;
+
+abstract class WPPB_Object {
+
+	/**
+	 * The ID of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string $plugin_name The ID of this plugin.
+	 */
+	protected $plugin_name;
+
+	/**
+	 * The version of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string $version The current version of this plugin.
+	 */
+	protected $version;
+
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @param string $plugin_name The name of this plugin.
+	 * @param string $version The version of this plugin.
+	 *
+	 * @since    1.0.0
+	 */
+	public function __construct( $plugin_name, $version ) {
+
+		$this->plugin_name = $plugin_name;
+		$this->version     = $version;
+
+	}
+
+	/**
+	 * The name of the plugin used to uniquely identify it within the context of
+	 * WordPress and to define internationalization functionality.
+	 *
+	 * @since     1.0.0
+	 * @return    string    The name of the plugin.
+	 */
+	public function get_plugin_name() {
+		return $this->plugin_name;
+	}
+	
+	/**
+	 * Retrieve the version number of the plugin.
+	 *
+	 * @since     1.0.0
+	 * @return    string    The version number of the plugin.
+	 */
+	public function get_version() {
+		return $this->version;
+	}
+
+
+}
